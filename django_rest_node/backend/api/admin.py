ab34150d621bb197aaa63abf0cfc4385d8ba0156
chanwoo4267 <chanwoo4267@naver.com>
2024-04-28 21:58:05 +0900

    Item deletion, deployment

--- django_rest_node/backend/api/admin.py
+++ django_rest_node/backend/api/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Item
 
-# Register your models here.
+@admin.register(Item)
+class ItemAdmin(admin.ModelAdmin):
+    pass  # You can customize the admin options for your model here
--- django_rest_node/backend/backend/settings.py
+++ django_rest_node/backend/backend/settings.py
@@ -49,11 +49,13 @@ CORS_ALLOW_CREDENTIALS = True
 CORS_ORIGIN_WHITELIST = (
     'http://127.0.0.1:3000',  # for localhost (REACT Default)
     'http://127.0.0.1:8000',  # for localhost (Developlemt)
+    'http://localhost:3000',
 )
 
 CSRF_TRUSTED_ORIGINS = [
     'http://127.0.0.1:3000',  # for localhost (REACT Default)
     'http://127.0.0.1:8000',  # for localhost (Developlemt)
+    'http://localhost:3000',
 ]
 
 MIDDLEWARE = [
